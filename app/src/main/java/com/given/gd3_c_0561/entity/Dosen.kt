685fdd95cdfc47b07d0fe35368fd5969cf642110
Ivan Kristiawan <ivanteens@gmail.com>
2022-09-08 16:29:56 +0700

    Init

---
+++ app/src/main/java/com/given/gd3_c_0561/entity/Dosen.kt
@@ -0,0 +1,12 @@
+package com.given.gd3_c_0561.entity
+
+class Dosen(var name:String, var pengajar:String) {
+
+    companion object{
+        @JvmField
+        var listofDosen= arrayOf(
+            Dosen("Fedelis Brian","Pengajar Kelas A, B, dan D"),
+            Dosen("Thomas Adi","Pengajar Kelas C"),
+        )
+    }
+}
